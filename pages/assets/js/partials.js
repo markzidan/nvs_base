324111f05418e86f1ee073f373826e33b1a6cb1d
markzidan <mark@becro.xyz>
2019-08-13 16:02:05 -0400

    git ignore issue fix-

---
+++ pages/assets/js/partials.js
@@ -0,0 +1,8 @@
+$("#header").load("partials.html #header-contents", "data", function (response, status, request) {
+    // this; // dom element
+    
+});
+$("#footer").load("partials.html #footer-contents", "data", function (response, status, request) {
+    // this; // dom element
+    
+});
